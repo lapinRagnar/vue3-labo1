e22c75347c92773888744e0d575c249965161850
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 12:11:35 +0100

    installation de vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 
-export default defineConfig({
-  plugins: [
-    
-  ]
-})
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
